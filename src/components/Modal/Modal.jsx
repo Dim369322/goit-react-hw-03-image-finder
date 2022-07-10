8372f40fec99c35b56fb9a8454a561962272edbb
Dim369322 <garnazhenkobb@gmail.com>
2022-07-10 11:41:40 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
-import PropTypes from 'prop-types';
 import styles from 'components/Modal/Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
